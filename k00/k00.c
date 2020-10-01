6986a3b46b0def5fb4071262ed8ab48a53731dd2
bb35319037 <kyo.1028.ping@gmail.com>
2020-10-01 10:39:56 +0900

    確認

--- k00/k00.c
+++ k00/k00.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+    int *p;
+    int i;
 
-int main(void)
-{
-{
-    printf("Hello World\n");
-}
-return 0;
+    p=(int*)malloc(sizeof(int)*5);
+    if(p == NULL){
+    }else{
+        for(i=0;i<5;i++){
+            *(p+i)=i+1;
+        }
+        for(i=0;i<5;i++){
+            printf("%d\n",*(p+i));
+        }
+        free(p);
+    }
 }
